723500761c5c0ccfa69537cdfdaa3560966964a9
JAY VARDHAN VASHISHTHA <jayvashishtha.agra@gmail.com>
2023-10-29 21:41:55 +0530

    HomePage added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:libralink/landingPage.dart';
+import 'package:libralink/Screens/Auth/login_screen.dart';
+import 'package:libralink/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LandingPage(),
+      home: HomePage(),
     );
   }
 }
