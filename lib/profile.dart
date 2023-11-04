8c6777d3a3bc324f2a2db6e7cc9a7ab5f4c77c6f
SHAKTI <spmshakti@gmail.com>
2023-11-04 16:59:56 +0530

    id_card_image_added

--- lib/profile.dart
+++ lib/profile.dart
@@ -17,39 +17,60 @@ class _ProfilePageState extends State<ProfilePage> {
   final TextEditingController idCardController = TextEditingController();
 
   File? _image;
+  File? idCardImage;
 
   Future<void> _pickImage() async {
-  final imagePicker = ImagePicker();
-  final pickedFile = await showModalBottomSheet<PickedFile>(
-    context: context,
-    builder: (BuildContext context) {
-      return Wrap(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.photo_library),
-            title: Text('Gallery'),
-            onTap: () async {
-              Navigator.pop(context, await imagePicker.pickImage(source: ImageSource.gallery));
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.photo_camera),
-            title: Text('Camera'),
-            onTap: () async {
-              Navigator.pop(context, await imagePicker.pickImage(source: ImageSource.camera));
-            },
-          ),
-        ],
-      );
-    },
-  );
+    final imagePicker = ImagePicker();
+    final pickedFile = await showModalBottomSheet<PickedFile>(
+      context: context,
+      builder: (BuildContext context) {
+        return Wrap(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.photo_library),
+              title: Text('Gallery'),
+              onTap: () async {
+                Navigator.pop(context);
+                  final image =  await imagePicker.pickImage(source: ImageSource.gallery);
+                 if (image != null) {
+                setState(() {
+                  _image = File(image.path);
+                });
+              }
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_camera),
+              title: Text('Camera'),
+              onTap: () async {
+                Navigator.pop(context);
+                 final image=  await imagePicker.pickImage(source: ImageSource.camera);
+               if (image != null) {
+                setState(() {
+                  _image = File(image.path);
+                });
+              }
+              },
+            ),
+          ],
+        );
+      },
+    );
 
-  if (pickedFile != null) {
-    setState(() {
-      _image = File(pickedFile.path);
-    });
   }
-}
+
+  Future<void> _pickIdCardImage() async {
+    final imagePicker = ImagePicker();
+    final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        idCardImage = File(pickedFile.path);
+        idCardController.text =
+            pickedFile.path; 
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,26 +105,45 @@ class _ProfilePageState extends State<ProfilePage> {
                   Center(
                     child: GestureDetector(
                       onTap: _pickImage,
-                      child: Container(
-                        width: 140,
-                        height: 140,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                        ),
-                        child: ClipOval(
-                          child: _image != null
-                              ? AspectRatio(
-                                  aspectRatio: 1.0,
-                                  child: Image.file(
-                                    _image!,
-                                    fit: BoxFit.cover,
-                                  ),
-                                )
-                              : Image.asset(
-                                  'assets/avatar.png',
+                      child: Stack(
+                        children: [
+                          Container(
+                            width: 140,
+                            height: 140,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                            ),
+                            child: ClipOval(
+                              child: _image != null
+                                  ? AspectRatio(
+                                      aspectRatio: 1.0,
+                                      child: Image.file(
+                                        _image!,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    )
+                                  : Image.asset(
+                                      'assets/avatar.png',
+                                      fit: BoxFit.cover,
+                                    ),
+                            ),
+                          ),
+                          if(_image == null)
+                          Positioned(
+                            bottom: 5,
+                            right: 5,
+                            child: Container(
+                              width: 35,
+                              height: 35,
+                              child: Center(
+                                child: Image.asset(
+                                  'assets/plus.png',
                                   fit: BoxFit.cover,
                                 ),
-                        ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
@@ -155,14 +195,52 @@ class _ProfilePageState extends State<ProfilePage> {
                   ),
                   SizedBox(height: 10),
 
-                  // ID Card
                   TextField(
                     controller: idCardController,
                     decoration: InputDecoration(
                       labelText: 'ID Card',
-                      suffixIcon: Icon(Icons.add_card),
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          
+                          _pickIdCardImage();
+                        },
+                        child: Icon(Icons.add_a_photo),
+                      ),
                     ),
                   ),
+                  SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                    
+                      if (idCardImage != null) {
+                        showDialog(
+                          context: context,
+                          builder: (_) {
+                            return AlertDialog(
+                              content: Image.file(idCardImage!),
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        if (idCardImage != null)
+                          Container(
+                            width: 150, 
+                            height: 100, 
+                            child: Image.file(idCardImage!, fit: BoxFit.cover),
+                          )
+                        else
+                          Text(
+                            'Click on Icon to Upload ID',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                      ],
+                    ),
+                  ),
+
                   SizedBox(height: 20),
                 ],
               ),
@@ -170,9 +248,7 @@ class _ProfilePageState extends State<ProfilePage> {
 
             // Save Button
             ElevatedButton(
-              onPressed: () {
-               
-              },
+              onPressed: () {},
               style: ElevatedButton.styleFrom(
                 primary: Colors.black,
                 shape: RoundedRectangleBorder(
