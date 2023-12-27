a8a7c3f1475b30ccd0135508e2bfcad666b1ff5d
Shubham Tiwari <125534462+ShubhamTiwari55@users.noreply.github.com>
2023-12-27 20:39:41 +0530

    Revert "added firebase locally and multidex enabled"

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
-import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
