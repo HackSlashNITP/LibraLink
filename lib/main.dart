17ba6c20f3883d7e3b982d330ceee0d3b0a96bde
ShubhamTiwari55 <stofficial059@gmail.com>
2023-12-30 21:37:07 +0530

    notification implementation completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:libralink/Screens/Auth/login_screen.dart';
+import 'package:libralink/firebase_api.dart';
 import 'package:libralink/firebase_options.dart';
 import 'package:libralink/home_screen.dart';
 import 'package:libralink/Screens/Add_books.dart';
@@ -15,11 +16,14 @@ import 'package:libralink/routes/mapping.dart';
 
 import 'Screens/Auth/signup_screen.dart';
 
+// Instantiate navigatorKey
+final navigatorKey = GlobalKey<NavigatorState>();
+
 Future<void> main() async {
-  runApp(const MyApp());
   WidgetsFlutterBinding.ensureInitialized();
-  
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotifications();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -27,17 +31,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, routes: {
-      "/": (context) => LandingPage(),
-      MyRoutes.homeRoute: (context) => HomePage(),
-      MyRoutes.issuedBooksRoute: (context) => IssuedBookPage(),
-      MyRoutes.pDuesRoute: (context) => DuesScreen(),
-      MyRoutes.nDuesRoute: (context) => noDues(),
-      MyRoutes.notifRoute: (context) => notification(),
-      MyRoutes.profileRoute: (context) => ProfilePage(),
-      MyRoutes.signinRoute: (context) => LoginScreen(),
-      MyRoutes.signupRoute: (context) => SignUpScreen(),
-
-    });
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
+      routes: {
+        "/": (context) => LandingPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.issuedBooksRoute: (context) => IssuedBookPage(),
+        MyRoutes.pDuesRoute: (context) => DuesScreen(),
+        MyRoutes.nDuesRoute: (context) => noDues(),
+        MyRoutes.notifRoute: (context) => NotificationScreen(),
+        MyRoutes.profileRoute: (context) => ProfilePage(),
+        MyRoutes.signinRoute: (context) => LoginScreen(),
+        MyRoutes.signupRoute: (context) => SignUpScreen(),
+      },
+    );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,13 @@ import Foundation
 import cloud_firestore
 import file_selector_macos
 import firebase_core
+import firebase_messaging
+import flutter_local_notifications
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
 }
